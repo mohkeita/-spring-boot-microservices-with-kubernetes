ca1a710881d9bb16964a45c1d024786d286a3db1
“mohkeita” <mohkeita13@gmail.com>
2024-01-03 23:55:39 +0100

    Implement openfeign

--- OrderService/src/main/java/io/mohkeita/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/io/mohkeita/OrderService/service/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package io.mohkeita.OrderService.service;
 
 import io.mohkeita.OrderService.entity.Order;
+import io.mohkeita.OrderService.external.client.ProductService;
 import io.mohkeita.OrderService.model.OrderRequest;
 import io.mohkeita.OrderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
@@ -15,10 +16,16 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductService productService;
     @Override
     public long placeOrder(OrderRequest orderRequest) {
         log.info("Placing Order Request: {}", orderRequest);
 
+        productService.reduceQuantity(orderRequest.getProductId(), orderRequest.getQuantity());
+
+        log.info("Created Order with Status CREATED");
         Order order = Order.builder()
                 .amount(orderRequest.getTotalAmount())
                 .orderStatus("CREATED")
