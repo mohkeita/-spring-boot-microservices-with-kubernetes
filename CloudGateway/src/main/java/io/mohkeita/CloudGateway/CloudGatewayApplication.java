20fc95e210d02562ce297faf877a7e65f8065b1e
“mohkeita” <mohkeita13@gmail.com>
2024-01-07 02:41:30 +0100

    implement rate limiter using resilience and redis

--- CloudGateway/src/main/java/io/mohkeita/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/io/mohkeita/CloudGateway/CloudGatewayApplication.java
@@ -2,11 +2,13 @@ package io.mohkeita.CloudGateway;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class CloudGatewayApplication {
@@ -15,6 +17,11 @@ public class CloudGatewayApplication {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
+	@Bean
+	KeyResolver userKeySolver() {
+		return exchange -> Mono.just("userKey");
+
+	}
 	@Bean
 	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
 
