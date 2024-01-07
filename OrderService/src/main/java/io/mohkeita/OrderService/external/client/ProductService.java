64362c6acfa41b9b47c78489005f843d7bd82f81
“mohkeita” <mohkeita13@gmail.com>
2024-01-07 01:40:10 +0100

    implement circuit breaker api gateway and in order service

--- OrderService/src/main/java/io/mohkeita/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/io/mohkeita/OrderService/external/client/ProductService.java
@@ -1,11 +1,13 @@
 package io.mohkeita.OrderService.external.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.mohkeita.OrderService.exception.CustomException;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/products")
 public interface ProductService {
 
@@ -14,4 +16,11 @@ public interface ProductService {
             @PathVariable("id") long productId,
             @RequestParam long quantity
     );
+
+    default void fallback(Exception e) {
+        throw new CustomException(
+                "Product Service is not available",
+                "UNAVAILABLE", 500
+        );
+    }
 }
